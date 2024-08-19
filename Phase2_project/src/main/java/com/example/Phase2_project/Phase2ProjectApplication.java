ff25fe824bfd375b67fe5d4f7b71f62f40979714
nagaPraveen317 <praveenkumarkolli2022@gmail.com>
2024-08-19 15:57:35 +0000

    first commit

---
+++ Phase2_project/src/main/java/com/example/Phase2_project/Phase2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Phase2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase2ProjectApplication.class, args);
+	}
+
+}
